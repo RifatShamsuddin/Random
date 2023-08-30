65ae368e462cc68324125fe49f15c91a2c2e44a1
RifatShamsuddin <60315803+RifatShamsuddin@users.noreply.github.com>
2023-08-30 14:07:35 +0600

    Open Site Script

---
+++ Open Site Script Auto Demo/open_site_script.py
@@ -0,0 +1,6 @@
+import webbrowser
+
+# Replace 'https://www.example.com' with the actual link you want to open
+link_to_open = 'https://www.example.com'
+
+webbrowser.get('msedge').open(link_to_open)
